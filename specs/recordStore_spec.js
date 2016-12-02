9bbb950e70ccf0eaf8877b12e9f58394b82bb081
RLOG89 <ross.loggie@gmail.com>
2016-12-02 13:36:29 +0000

    tested constructor

--- specs/recordStore_spec.js
+++ specs/recordStore_spec.js
@@ -1,14 +1,16 @@
 var RecordStore = require('../RecordStore');
 var assert = require('assert');
 
+var ricks = new RecordStore("Ricks", "Glasgow")
+
 describe('RecordStore', function() {
 
   it('should have a name', function() {
-    assert.equal()
+    assert.equal("Ricks", ricks.name)
   });
 
   it('should have a city', function() {
-    assert.equal()
+    assert.equal("Glasgow", ricks.city)
   });
-  
+
 });
