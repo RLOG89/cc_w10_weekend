fd6816e6618b5b2c54801d1e4d7e7e6158120545
RLOG89 <ross.loggie@gmail.com>
2016-12-02 13:30:58 +0000

    created basic tests

---
+++ specs/recordStore_spec.js
@@ -0,0 +1,14 @@
+var RecordStore = require('../RecordStore');
+var assert = require('assert');
+
+describe('RecordStore', function() {
+
+  it('should have a name', function() {
+    assert.equal()
+  });
+
+  it('should have a city', function() {
+    assert.equal()
+  });
+  
+});
