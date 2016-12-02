eda326c95cb943c04e5a851d885c19c94f6d4f40
RLOG89 <ross.loggie@gmail.com>
2016-12-02 13:24:30 +0000

    wrote basic tests

--- specs/record_spec.js
+++ specs/record_spec.js
@@ -3,5 +3,17 @@ var assert = require('assert');
 
 describe('record', function() {
 
-  it('should have an artist')
-})
+  it('should have an artist', function() {
+    assert.equal()
+  });
+
+  it('should have a title', function() {
+    assert.equal()
+  });
+
+  it('should have a price', function() {
+    assert.equal()
+  });
+  
+});
+
